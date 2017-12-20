f31ee96b8092ac98c5eeb787a71a168c63d9a1b6
Luong Mai Van <luongmv@dtn.com.vn>
2017-12-20 11:13:19 +0700

    Add Comment feature

---
+++ app/Comment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table='comment';
+
+    public function product(){
+        return $this->belongsTo('App\Product','product_id','id');
+    }
+}
---
+++ database/migrations/2017_09_07_081831_create_comment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->nullable();
+            $table->string('email');
+            $table->string('name');
+            $table->integer('product_id')->unsigned();
+            $table->text('description');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
