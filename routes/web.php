4f9323e1e4ce232d03c8438efdebe4e344924139
Luong Mai Van <maivanluong1@gmail.com>
2018-03-26 11:34:52 +0700

    Format code of Route

--- routes/web.php
+++ routes/web.php
@@ -11,131 +11,65 @@
 |
 */
 
-Route::get('/', [
-   'as' => 'home',
-   'uses' => 'HomeController@index'
-]);
+/*******Home**************************/
+Route::get('/', ['as' => 'home','uses' => 'HomeController@index']);
 
-Route::get('product/{id}-{slug}',[
-   'as' =>'product.view',
-   'uses'=> 'ProductController@view' 
-]);
+/******Checkout***********************/
+Route::group(['prefix' => 'checkout'], function (){
+    Route::get('/cart',['as' =>'checkout.cart','uses'=> 'CartController@checkout']);
+    Route::post('/step2',['as' =>'checkout.step2','uses'=> 'CheckoutController@step2']);
+    Route::get('/step2',['as' =>'checkout.step2','uses'=> 'CartController@checkout']);
+    Route::post('/step3',['as' =>'checkout.step3','uses'=> 'CheckoutController@step3']);
+    Route::get('/step3',['as' =>'checkout.step3','uses'=> 'CartController@checkout']);
+    Route::post('/step4',['as' =>'checkout.step4','uses'=> 'CheckoutController@step4']);
+    Route::get('/step4',['as' =>'checkout.step4','uses'=> 'CartController@checkout']);
+    Route::post('/step5',['as' =>'checkout.step5','uses'=> 'CheckoutController@step5']);
+    Route::post('/success',['as' =>'checkout.step6','uses'=> 'CheckoutController@step6']);
+    Route::get('/success',['as' =>'checkout.step6','uses'=> 'CheckoutController@successPage']);
+    Route::post('/order',['as' =>'checkout.order','uses'=> 'CheckoutController@order']);
+});
 
-Route::get('cart/view',[
-    'as' =>'cart.view',
-    'uses'=> 'CartController@view'
-]);
+/******Cart**************************/
+Route::group(['prefix' => 'cart'], function (){
+    Route::post('/add/{type?}',['as' => 'cart.add','uses' => 'CartController@addToCart']);
+    Route::get('/remove/{id}',['as'=> 'cart.remove','uses'=> 'CartController@removeItem']);
+    Route::get('/view',['as' =>'cart.view','uses'=> 'CartController@view']);
+});
 
-Route::get('checkout/cart',[
-    'as' =>'checkout.cart',
-    'uses'=> 'CartController@checkout'
-]);
+/******Category*********************/
+Route::group(['prefix' => 'category'], function (){
+    Route::get('/gird/{id?}', ['as' => 'category.productgird', 'uses' => 'CategoryController@productgird'])->where('id', '[0-9]+');
+    Route::get('/list/{id?}', ['as' => 'category.productlist', 'uses' => 'CategoryController@productlist']);
+});
 
-Route::post('checkout/step2',[
-    'as' =>'checkout.step2',
-    'uses'=> 'CheckoutController@step2'
-]);
-Route::get('checkout/step2',[
-    'as' =>'checkout.step2',
-    'uses'=> 'CartController@checkout'
-]);
+/******Product***********************/
+Route::group(['prefix' => 'product'], function () {
+    Route::get('/{id}-{slug}', ['as' => 'product.view', 'uses' => 'ProductController@view']);
+});
 
-Route::post('checkout/step3',[
-    'as' =>'checkout.step3',
-    'uses'=> 'CheckoutController@step3'
-]);
-Route::get('checkout/step3',[
-    'as' =>'checkout.step3',
-    'uses'=> 'CartController@checkout'
-]);
+/******Customer********************/
+Route::group(['prefix' => 'customer'], function (){
+    Route::get('/register',['as' => 'customer.register','uses' => 'CustomerController@form_register']);
+    Route::post('/register',['as' => 'customer.register','uses' => 'CustomerController@register']);
+    Route::get('/login',['as' => 'customer.login','uses' => 'CustomerController@form_login']);
+    Route::post('/login',['as' => 'customer.login','uses' => 'CustomerController@login']);
+    Route::get('/logout',['as' => 'customer.logout','uses' => 'CustomerController@logout']);
+    Route::get('/profile/{id}',['as' => 'customer.view.profile','uses' => 'CustomerController@profile']);
+});
 
-Route::post('checkout/step4',[
-    'as' =>'checkout.step4',
-    'uses'=> 'CheckoutController@step4'
-]);
-Route::get('checkout/step4',[
-    'as' =>'checkout.step4',
-    'uses'=> 'CartController@checkout'
-]);
-Route::post('checkout/step5',[
-    'as' =>'checkout.step5',
-    'uses'=> 'CheckoutController@step5'
-]);
+/******Search********************/
+Route::group(['prefix' => 'search'], function (){
+    Route::post('/{category?}',['as' => 'search','uses' => 'SearchController@search']);
+    Route::get('/{category?}',['as' => 'search','uses' => 'SearchController@search']);
+});
 
-Route::post('checkout/success',[
-    'as' =>'checkout.step6',
-    'uses'=> 'CheckoutController@step6'
-]);
-Route::get('checkout/success',[
-    'as' =>'checkout.step6',
-    'uses'=> 'CheckoutController@successPage'
-]);
-
-Route::post('checkout/order',[
-    'as' =>'checkout.order',
-    'uses'=> 'CheckoutController@order'
-]);
-
-Route::get('category/gird/{id?}',[
-    'as' =>'category.productgird',
-    'uses'=> 'CategoryController@productgird'
-])->where('id', '[0-9]+');
-
-Route::get('category/list/{id?}',[
-    'as' =>'category.productlist',
-    'uses'=> 'CategoryController@productlist'
-]);
-
-//Customer---------------------------------------------------------
-Route::get('customer/register',[
-    'as' => 'customer.register',
-    'uses' => 'CustomerController@form_register'
-]);
-
-Route::post('customer/register',[
-    'as' => 'customer.register',
-    'uses' => 'CustomerController@register'
-]);
-
-Route::get('customer/login',[
-    'as' => 'customer.login',
-    'uses' => 'CustomerController@form_login'
-]);
-
-Route::post('customer/login',[
-    'as' => 'customer.login',
-    'uses' => 'CustomerController@login'
-]);
-
-Route::get('customer/logout',[
-    'as' => 'customer.logout',
-    'uses' => 'CustomerController@logout'
-]);
-
-Route::get('customer/profile/{id}',[
-    'as' => 'customer.view.profile',
-    'uses' => 'CustomerController@profile'
-]);
-
-//Seach--------------------------------------
-Route::post('search/{category?}',[
-    'as' => 'search',
-    'uses' => 'SearchController@search'
-]);
-Route::get('search/{category?}',[
-   'as' => 'search',
-  'uses' => 'SearchController@search'
-]);
-
-//Comment----------------------------------------
-
-Route::post('comment/add',[
-   'as' => 'comment.add',
-    'uses' => 'CommentController@add'
-]);
+/******Comment********************/
+Route::group(['prefix' => 'comment'], function () {
+    Route::post('/add', ['as' => 'comment.add','uses' => 'CommentController@add']);
+});
 
 /**
- * ***********ADMIN*************************************************************************************************************************************************************
+ * ***********ADMIN***********************************************************************************
  */
 Route::get('admin/login',[
     'as' => 'admin.login',
@@ -145,74 +79,33 @@ Route::post('admin/login',[
     'as' => 'admin.login',
     'uses'=>'Admin\DashboardController@loginPost'
 ]);
-
 Route::group(['middleware' => ['admin']],function (){
-    Route::get('admin/index',[
-        'as' => 'dashboard',
-        'uses'=>'Admin\DashboardController@dashboard'
-    ]);
-//category*******************************************************************//
-    Route::get('admin/category/list',[
-        'as' => 'admin.category.list',
-        'uses'=>'Admin\CategoryController@listCategory'
-    ]);
-    Route::get('admin/category/create/{id?}',[
-        'as' => 'admin.category.create',
-        'uses'=>'Admin\CategoryController@formCreate'
-    ]);
+    Route::group(['prefix' => 'admin'], function (){
+        Route::get('/index',['as' => 'dashboard','uses'=>'Admin\DashboardController@dashboard']);
 
-    Route::post('admin/category/create',[
-        'as' => 'admin.category.postcreate',
-        'uses'=>'Admin\CategoryController@create'
-    ]);
+    //category**********************************************************
+        Route::get('/category/list',['as' => 'admin.category.list','uses'=>'Admin\CategoryController@listCategory']);
+        Route::get('/category/create/{id?}',['as' => 'admin.category.create','uses'=>'Admin\CategoryController@formCreate']);
+        Route::post('/category/create',['as' => 'admin.category.postcreate','uses'=>'Admin\CategoryController@create']);
+        Route::get('/category/delete/{id?}',['as' => 'admin.category.delete','uses'=>'Admin\CategoryController@delete']);
 
-    Route::get('admin/category/delete/{id?}',[
-        'as' => 'admin.category.delete',
-        'uses'=>'Admin\CategoryController@delete'
-    ]);
-//product***********************************************************
-    Route::get('admin/product/list',[
-        'as' => 'admin.product.list',
-        'uses'=>'Admin\ProductController@listProduct'
-    ]);
-    Route::get('admin/product/create/{id?}',[
-        'as' => 'admin.product.create',
-        'uses'=>'Admin\ProductController@formCreate'
-    ]);
-    Route::post('admin/product/create',[
-        'as' => 'admin.product.postcreate',
-        'uses'=>'Admin\ProductController@create'
-    ]);
-    Route::get('admin/product/delete/{id?}',[
-        'as' => 'admin.product.delete',
-        'uses'=>'Admin\ProductController@delete'
-    ]);
-    //Customer
-    Route::get('admin/customer/list',[
-        'as' => 'admin.customer',
-        'uses'=> 'Admin\CustomerController@listCustomer'
-    ]);
+    //product***********************************************************
+        Route::get('/product/list',['as' => 'admin.product.list','uses'=>'Admin\ProductController@listProduct']);
+        Route::get('/product/create/{id?}',['as' => 'admin.product.create','uses'=>'Admin\ProductController@formCreate']);
+        Route::post('/product/create',['as' => 'admin.product.postcreate','uses'=>'Admin\ProductController@create']);
+        Route::get('/product/delete/{id?}',['as' => 'admin.product.delete','uses'=>'Admin\ProductController@delete']);
 
-//orders********************************************************
-    Route::get('admin/order/list',[
-        'as' => 'admin.orders.list',
-        'uses'=> 'Admin\OrdersController@listOrders'
-    ]);
-    Route::get('admin/order/view/{id}',[
-        'as' => 'admin.orders.view',
-        'uses'=> 'Admin\OrdersController@view'
-    ]);
+    //Customer**********************************************************
+        Route::get('/customer/list',['as' => 'admin.customer','uses'=> 'Admin\CustomerController@listCustomer']);
+
+    //orders************************************************************
+        Route::get('/order/list',['as' => 'admin.orders.list','uses'=> 'Admin\OrdersController@listOrders']);
+        Route::get('/order/view/{id}',['as' => 'admin.orders.view','uses'=> 'Admin\OrdersController@view']);
+    });
 });
-
-//cart********************************************************
-Route::post('cart/add/{type?}',[
-    'as' => 'cart.add',
-    'uses' => 'CartController@addToCart'
-]);
-Route::get('cart/remove/{id}',[
-    'as'=> 'cart.remove',
-    'uses'=> 'CartController@removeItem'
-]);
+/**
+ * *********EndAdmin****************************************************************************************
+ */
 
 
 
