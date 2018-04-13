4eda0528a2088a7c5879eaa83e173687ca23dfd8
Luong Mai Van <maivanluong1@gmail.com>
2018-04-13 11:40:07 +0700

    Update Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,5 +14,152 @@
                 <li class="active">Dashboard</li>
             </ol>
         </section>
+        <section class="content">
+            <!-- Info boxes -->
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">CPU Traffic</span>
+                            <span class="info-box-number">90<small>%</small></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Likes</span>
+                            <span class="info-box-number">41,410</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+
+                <!-- fix for small devices only -->
+                <div class="clearfix visible-sm-block"></div>
+
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Sales</span>
+                            <span class="info-box-number">760</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">New Members</span>
+                            <span class="info-box-number">2,000</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- Chat box -->
+            <div class="box box-success">
+                <div class="box-header">
+                    <i class="fa fa-comments-o"></i>
+
+                    <h3 class="box-title">Chat</h3>
+
+                    <div class="box-tools pull-right" data-toggle="tooltip" title="Status">
+                        <div class="btn-group" data-toggle="btn-toggle">
+                            <button type="button" class="btn btn-default btn-sm active"><i class="fa fa-square text-green"></i>
+                            </button>
+                            <button type="button" class="btn btn-default btn-sm"><i class="fa fa-square text-red"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-body chat" id="chat-box">
+                    <!-- chat item -->
+                    <div class="item">
+                        <img src="dist/img/user4-128x128.jpg" alt="user image" class="online">
+
+                        <p class="message">
+                            <a href="#" class="name">
+                                <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small>
+                                Mike Doe
+                            </a>
+                            I would like to meet you to discuss the latest news about
+                            the arrival of the new theme. They say it is going to be one the
+                            best themes on the market
+                        </p>
+                        <div class="attachment">
+                            <h4>Attachments:</h4>
+
+                            <p class="filename">
+                                Theme-thumbnail-image.jpg
+                            </p>
+
+                            <div class="pull-right">
+                                <button type="button" class="btn btn-primary btn-sm btn-flat">Open</button>
+                            </div>
+                        </div>
+                        <!-- /.attachment -->
+                    </div>
+                    <!-- /.item -->
+                    <!-- chat item -->
+                    <div class="item">
+                        <img src="dist/img/user3-128x128.jpg" alt="user image" class="offline">
+
+                        <p class="message">
+                            <a href="#" class="name">
+                                <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 5:15</small>
+                                Alexander Pierce
+                            </a>
+                            I would like to meet you to discuss the latest news about
+                            the arrival of the new theme. They say it is going to be one the
+                            best themes on the market
+                        </p>
+                    </div>
+                    <!-- /.item -->
+                    <!-- chat item -->
+                    <div class="item">
+                        <img src="dist/img/user2-160x160.jpg" alt="user image" class="offline">
+
+                        <p class="message">
+                            <a href="#" class="name">
+                                <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 5:30</small>
+                                Susan Doe
+                            </a>
+                            I would like to meet you to discuss the latest news about
+                            the arrival of the new theme. They say it is going to be one the
+                            best themes on the market
+                        </p>
+                    </div>
+                    <!-- /.item -->
+                </div>
+                <!-- /.chat -->
+                <div class="box-footer">
+                    <div class="input-group">
+                        <input class="form-control" placeholder="Type message...">
+
+                        <div class="input-group-btn">
+                            <button type="button" class="btn btn-success"><i class="fa fa-plus"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.box (chat box) -->
+        </section>
     </div>
 @stop
