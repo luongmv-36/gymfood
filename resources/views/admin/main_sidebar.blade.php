a98383d88a3a40aa9bd2dcbead4e1ab4a092da40
Luong Mai Van <maivanluong1@gmail.com>
2018-04-13 15:55:00 +0700

    Update Logo In Admin

--- resources/views/admin/main_sidebar.blade.php
+++ resources/views/admin/main_sidebar.blade.php
@@ -5,7 +5,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{URL::to('/')}}/adminhtml/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="{{URL::to('/')}}/adminhtml/dist/img/gymie-logo-small.png" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
                 <p>{{Auth::user()->name}}</p>
